d1c353a728996a31c1213359a55a4ab73cda5d69
jemmyasjd <jemmy33.jd@gmail.com>
2024-10-20 12:01:38 +0530

    initial commit

---
+++ backend/Model/todoSchema.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const todoSchema = new mongoose.Schema({
+        userID:{
+            type : mongoose.Schema.Types.ObjectId,
+            ref : "todouser",
+            Required :true,
+        },
+    Title:{
+        type : String,
+        Required : true,
+    },
+    Tasks: [String]
+},
+{
+    timestamps : true
+});
+
+module.exports = mongoose.model("todo",todoSchema);
